93f370ae26332f63cd77f9906945a40a502f4701
Gabriel Pires <gabpiresdev@gmail.com>
2022-05-10 20:16:53 -0300

    feat: new header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
 export const App = () => {
   return (
-    <div className="App">
+    <>
+      <Header />
       <GlobalStyle />
-      <h1>Hello world</h1>
-    </div>
+    </>
   );
 };
