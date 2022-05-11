cf6cd98f1d8a7a1d639b65ce2c7ea4bca4f26cf7
Gabriel Pires <gabpiresdev@gmail.com>
2022-05-11 18:55:21 -0300

    feat: fetching transactions data from api

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,36 @@ createServer({
   models: {
     transaction: Model,
   },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de website',
+          amount: 12000,
+          category: 'Desenvolvimento',
+          type: 'deposit',
+          createdAt: new Date('2021-02-20'),
+        },
+        {
+          id: 2,
+          title: 'Desenvolvimento de aplicativo',
+          amount: 10000,
+          category: 'Desenvolvimento',
+          type: 'deposit',
+          createdAt: new Date('2021-02-20'),
+        },
+        {
+          id: 3,
+          title: 'Aluguel',
+          amount: 1000,
+          category: 'Aluguel',
+          type: 'withdraw',
+          createdAt: new Date('2021-02-18'),
+        },
+      ],
+    });
+  },
   routes() {
     this.namespace = 'api';
 
